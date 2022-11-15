1e9a20ea73b1537ae93ac995592d3e455ed9887a
jmykkane <jmykkane@student.hive.fi>
2022-11-15 15:48:14 +0200

    messed up, went back before leak fixes and started checking overflow reading from mallocs.

--- test.c
+++ test.c
@@ -1,4 +1,3 @@
-
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
@@ -6,8 +5,22 @@
 #include <stdio.h>
 #include "get_next_line.h"
 #include <string.h>
-#include <stdio.h>
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 15
+#endif
+
+size_t	ft_strlen(char const *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
@@ -28,16 +41,27 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 
-size_t	ft_strlen(char const *s)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	int	i;
+	int				i;
+	unsigned char	ch;
+	void			*ptr;
+	void			*src;
 
-	if (!s)
-		return (0);
 	i = 0;
-	while (s[i] != '\0')
+	ch = c;
+	src = (void *)s;
+	while (n > 0)
+	{
+		if (*((unsigned char *)s + i) == ch)
+		{
+			ptr = &src[i];
+			return (ptr);
+		}
+		n--;
 		i++;
-	return (i);
+	}
+	return (NULL);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
@@ -68,14 +92,179 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (str);
 }
 
+char	*handle_overflow(unsigned int ret, char *temp, int *index)
+{
+	unsigned int	len;
+	char 			*storage;
 
-int main () {
-   char *s1 = "Test";
-   char *s2 = NULL;
+	len = 0;
+	while (temp[len] != '\n' && len <= ret)
+	{
+		*index += 1;
+		len++;
+	}
+	if (len < 1)
+		return (NULL);
+	*index += 1;
+	len++;
+	len = ret - len;
+	storage = malloc(sizeof(char) * (len + 1));
+	if (!storage || len < 1)
+	{
+		free(storage);
+		return (NULL);
+	}
+	ft_memcpy(storage, temp + *index, len);
+	storage[len] = '\0';
+	return (storage);
+}
 
-   char *s3 = malloc(sizeof(char) * 50);
-   s3 = ft_strjoin(s2, s1);
-   printf("%s", s3);
-   
-   return(0);
+char	*build_str(char *storage, char *temp,unsigned int ret, int *trigger)
+{
+	static int	index = 0;
+	char		*str;
+	char		c;
+	int			i;
+
+	i = 0;
+	c = temp[i];
+	str = malloc(sizeof(char) * (index + ret + 1));
+	ft_memcpy(str, storage, index);
+	while (i < ret)
+	{
+		c = temp[i];
+		str[index + i] = c;
+		if (c == '\n')
+		{
+			str[index + i + 1] = '\0';
+			*trigger = 1;
+			index = 0;
+			return (str);
+		}
+		i++;
+	}
+	index += i;
+	return (str);
+}
+
+char	*read_line(int fd, char **buff, int	*index)
+{
+	unsigned int		ret;
+	char	*temp;
+	int		trigger;
+	char	*storage;
+	char	buf[BUFFER_SIZE];
+
+	trigger = 0;
+	ret = read(fd, buf, BUFFER_SIZE);
+	if (!ret)
+		return (NULL);
+	temp = malloc(sizeof(char) * ret);
+	if (!temp)
+		return (NULL);
+	while (ret)
+	{
+		ft_memcpy(temp, buf, ret);
+		storage = build_str(storage, temp, ret, &trigger);
+		if (trigger == 1)
+		{
+			*buff = handle_overflow(ret, temp, index);
+			*index = 0;
+			return (storage);
+		}
+		ret = read(fd, buf, BUFFER_SIZE);
+	}
+	return (storage);
+}
+
+char	*line_from_buff(char **buff, int len)
+{
+	char	*line;
+	char 	*temp;
+	int		size;
+
+	size = ft_strlen(*buff) - len;
+	line = malloc(sizeof(char) * (len + 2));
+	temp = malloc(sizeof(char) * (size + 1));
+	if (!line || !temp)
+		return (NULL);
+	ft_memcpy(line, *buff, len + 1);
+	line[len + 2] = '\0';
+	ft_memcpy(temp, *buff + len + 1, size);
+	temp[size + 1] = '\0';
+	free(*buff);
+	*buff = malloc(sizeof(char) * (size + 1));
+	ft_memcpy(*buff, temp, size + 1);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buff;
+	static int	index;
+	char		*line;
+	char		*storage;
+	int			len;
+
+	len = 0;
+	if (buff)
+	{
+		if (ft_memchr(buff, '\n', ft_strlen(buff)))
+		{
+			while (buff[len] != '\n')
+				len++;
+			line = line_from_buff(&buff, len);
+			return (line);
+		}
+		line = malloc(sizeof(char) * ft_strlen(buff) + 1);
+		ft_memcpy(line, buff, ft_strlen(buff) + 1);
+		storage = read_line(fd, &buff, &index);
+		if (len == 0 && ft_strlen(storage) == 0)
+			return (NULL);
+		return (ft_strjoin(line, storage));
+	}
+	else
+		storage = read_line(fd, &buff, &index);
+	return (storage);
+}
+
+
+int	main()
+{
+	int		fd;
+	char	*file;
+	char 	*line;
+	
+	file = "files/file.txt";
+	fd = open(file, O_RDONLY);
+
+	line = get_next_line(fd);
+	printf("%s", line);
+	free(line);
+	while (line != NULL)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		free(line);
+	}
+
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	
+	close(fd);
+	return (0);
 }
