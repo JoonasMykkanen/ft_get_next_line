ca1a88d5c366b11e9a5fac656d9de772c2d29b20
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2022-11-20 19:24:39 +0200

    read line finally under 25 lines

--- test.c
+++ test.c
@@ -169,7 +169,7 @@ static char	*build_str(char *storage, char *temp, int ret, int *trigger)
 	str = malloc(sizeof(char) * (index + ret + 2));
 	if (!str)
 		return(NULL);
-	if (*trigger == 42)
+	if (ft_strncmp(temp, "42", 2) == 0)
 	{
 		ft_memcpy(str, storage, ret);
 		str[ret] = '\0';
@@ -194,12 +194,11 @@ static char	*build_str(char *storage, char *temp, int ret, int *trigger)
 	return (str);
 }
 
-static char	*read_line(int fd, int ret)
+static char	*read_line(int fd, int ret, char *buf)
 {
 	char	*temp;
 	int		trigger;
 	char	*storage;
-	char	buf[BUFFER_SIZE];
 
 	trigger = 0;
 	storage = NULL;
@@ -210,10 +209,7 @@ static char	*read_line(int fd, int ret)
 	{
 		temp = NULL;
 		if (ft_strlen(storage) != 0)
-		{
-			trigger = 42;
-			temp = build_str(storage, temp, ft_strlen(storage), &trigger);
-		}
+			temp = build_str(storage, "42", ft_strlen(storage), &trigger);
 		free(storage);
 		storage = build_str(temp, buf, ret, &trigger);
 		free(temp);
@@ -227,75 +223,7 @@ static char	*read_line(int fd, int ret)
 	return (storage);
 }
 
-// static char	*build_str(char *storage, char *temp, unsigned int ret, int *trigger)
-// {
-// 	static int		index = 0;
-// 	char			*str;
-// 	char			c;
-// 	unsigned int	i;	
-
-// 	i = 0;
-// 	c = temp[i];
-// 	str = malloc(sizeof(char) * (index + ret + 2));
-// 	if (!str)
-// 		return(NULL);
-// 	if (storage)
-// 		ft_memcpy(str, storage, index);
-// 	while (i < ret)
-// 	{
-// 		c = temp[i];
-// 		str[index + i] = c;
-// 		if (c == '\n')
-// 		{
-// 			str[index + i + 1] = '\0';
-// 			*trigger = 1;
-// 			index = 0;
-// 			return (str);
-// 		}
-// 		i++;
-// 	}
-// 	str[index + i + 1] = '\0';
-// 	index += i;
-// 	return (str);
-// }
-
-// static char	*read_line(int fd, int ret)
-// {
-// 	char	*temp;
-// 	int		trigger;
-// 	char	*storage;
-// 	char	buf[BUFFER_SIZE];
-
-// 	trigger = 0;
-// 	storage = NULL;
-// 	ret = read(fd, buf, BUFFER_SIZE);
-// 	if (ret == -1)
-// 		return (NULL);
-// 	while (ret)
-// 	{
-// 		temp = malloc(sizeof(char) * ft_strlen(storage) + 2); //mby move to if statement
-// 		if (ft_strlen(storage) != 0)
-// 		{
-// 			ft_memcpy(temp, storage, ft_strlen(storage));			
-// 			temp[ft_strlen(storage) + 1] = '\0';
-// 			free(storage);
-// 			storage = NULL;
-// 		}
-// 		storage = build_str(temp, buf, ret, &trigger);
-// 		free(temp);
-// 		if (trigger == 1)
-// 		{
-// 			handle_overflow(ret, buf);
-// 			s.index = 0;
-// 			return (storage);
-// 		}
-// 		ret = read(fd, buf, BUFFER_SIZE);
-// 	}
-// 	return (storage);
-// }
-
 char	*line_from_buff(int len)
-
 {
 	char	*line;
 	char 	*temp;
@@ -327,20 +255,17 @@ char	*line_from_buff(int len)
 
 char	*get_next_line(int fd)
 {
+	char		buf[BUFFER_SIZE];
 	char		*storage;
 	char		*line;
 	char		*temp;
-	int			len;
-	int			ret;
 
-	ret = 0;
-	len = -1;
 	if (s.buff)
 	{
 		if (ft_memchr(s.buff, '\n', ft_strlen(s.buff)))
-			return (line_from_buff(len));
-		line = line_from_buff(len);
-		storage = read_line(fd, ret);
+			return (line_from_buff(-1));
+		line = line_from_buff(-1);
+		storage = read_line(fd, 0, buf);
 		if (storage == NULL && ft_strlen(line) < 1)
 		{
 			free(line);
@@ -352,7 +277,7 @@ char	*get_next_line(int fd)
 		free(storage);
 		return (temp);
 	}
-	return (read_line(fd, ret));
+	return (read_line(fd, 0, buf));
 }
 
 int	main()
